c477b0a98a9d984c033c2c2867c5f31d14cef0ed
EasyHub Global <scribesoul.a@gmail.com>
2025-11-14 03:36:06 +0000

    Add Tailwind directives to fix styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './minimal-styles.css'; // THIS FIRST!
-import './index.css';
+import './index.css'; // THIS FIRST!
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
