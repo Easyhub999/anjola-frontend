a3fe1e559312a819c9e67dfb69676c285d128550
EasyHub Global <scribesoul.a@gmail.com>
2025-11-16 22:54:10 +0000

    Clean API URL config

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 // API BASE URL
 // ==========================================
 
-const API_URL = process.env.REACT_APP_API_URL || "https://anjola-backend-2.onrender.com/api";
+const API_URL = process.env.REACT_APP_API_URL;
 
 
 // ==========================================
