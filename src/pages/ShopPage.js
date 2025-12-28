e1638b013b41b9ef53d05331932475872b21a97b
Easyhub Dev <scribesoul.a@gmail.com>
2025-12-28 15:34:48 -0800

    update

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -309,7 +309,7 @@ const ShopPage = ({
   };
 
   return (
-    <div id="shop-top" className="min-h-screen bg-gradient-to-br from-pink-50 via-white to-purple-50 pt-0 pb-16 -mt-12">
+    <div id="shop-top" className="min-h-screen bg-gradient-to-br from-pink-50 via-white to-purple-50 pt-0 pb-16 -mt-10">
       {/* Payment Success Modal */}
       {showPaymentSuccess && (
         <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
